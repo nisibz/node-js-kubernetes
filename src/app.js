76ece1d10caf81950e4396b941125bb89f39a606
nantachai <bz.nantachai@gmail.com>
2023-11-14 19:02:57 +0700

    add ingrees

--- src/app.js
+++ src/app.js
@@ -1,30 +1,22 @@
 const packageJson = require("../package.json");
 const express = require("express");
-const Docker = require("dockerode");
-const docker = new Docker();
 const app = express();
 
 const PORT = 3000;
 
-let counter = 0;
+const HOSTNAME = process.env.HOSTNAME || "localhost";
 
-app.get("/version", (req, res) => {
-  counter++;
-  console.log(`Counter: ${counter}`);
-  res.send(`Version ${packageJson.version} - ${counter}`);
+app.get("/", async (_req, res) => {
+  res.send({
+    version: packageJson.version,
+    containerId: HOSTNAME,
+  });
 });
 
-app.get("/container-id", async (req, res) => {
-  try {
-    const container = await docker.getContainer(process.env.HOSTNAME);
-    const containerId = container.id;
-    res.send(`Container ID: ${containerId}`);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error getting container ID");
-  }
+app.get("/health", async (_req, res) => {
+  res.sendStatus(200);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${HOSTNAME} Port: ${PORT}`);
 });
