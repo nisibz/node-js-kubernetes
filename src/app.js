569e728575563a38bd2f267baa82ecedf6a121ca
nantachai <bz.nantachai@gmail.com>
2023-11-14 12:51:30 +0700

    add api get container id

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const Docker = require("dockerode");
+const docker = new Docker();
 const app = express();
 
 const PORT = 3000;
@@ -7,7 +9,18 @@ let counter = 0;
 
 app.get("/version", (req, res) => {
   counter++;
-  res.send(`Version 1.0.0 - ${counter}`);
+  res.send(`Version 1.0.1 - ${counter}`);
+});
+
+app.get("/container-id", async (req, res) => {
+  try {
+    const container = await docker.getContainer(process.env.HOSTNAME);
+    const containerId = container.id;
+    res.send(`Container ID: ${containerId}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error getting container ID");
+  }
 });
 
 app.listen(PORT, () => {
